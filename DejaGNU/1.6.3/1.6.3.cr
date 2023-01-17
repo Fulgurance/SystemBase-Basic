73e7bfce499bdbefc0780476d91a14fc0ac1cdbb
fulgurance <zohran.londais@gmail.com>
2023-01-17 11:32:08 +0000

    Finished to implement DejaGNU installer

---
+++ DejaGNU/1.6.3/1.6.3.cr
@@ -0,0 +1,33 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+        configureSource([   "--prefix=#{Ism.settings.rootPath}/usr"],
+                            buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+        fileMakeinfo([  "--html",
+                        "--no-split",
+                        "-o",
+                        "doc/dejagnu.html",
+                        "../doc/dejagnu.texi"],
+                        buildDirectoryPath)
+        fileMakeinfo([  "--plaintext",
+                        "-o",
+                        "doc/dejagnu.txt",
+                        "../doc/dejagnu.texi"],
+                        buildDirectoryPath)
+        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}","install"],buildDirectoryPath)
+        makeDirectory("#{Ism.settings.rootPath}/usr/share/doc/dejagnu-1.6.3")
+        moveFile("#{buildDirectoryPath}/doc/dejagnu.html","#{Ism.settings.rootPath}/usr/share/doc/dejagnu-1.6.3/dejagnu.html")
+        moveFile("#{buildDirectoryPath}/doc/dejagnu.txt","#{Ism.settings.rootPath}/usr/share/doc/dejagnu-1.6.3/dejagnu.txt")
+    end
+
+end
