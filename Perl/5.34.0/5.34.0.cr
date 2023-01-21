4b2e8a31f33782695183a071f19f331568fccf62
fulgurance <zohran.londais@gmail.com>
2023-01-21 17:39:13 +0000

    Patched error for Perl

--- Perl/5.34.0/5.34.0.cr
+++ Perl/5.34.0/5.34.0.cr
@@ -3,23 +3,23 @@ class Target < ISM::Software
     def configure
         super
         @useChroot=true
-        runScript("Configure",buildDirectoryPath,
-                    [   "BUILD_ZLIB=False",
-                        "BUILD_BZIP2=0",
-                        "-des",
-                        "-Dprefix=/usr",
-                        "-Dvendorprefix=/usr",
-                        "-Dprivlib=/usr/lib/perl5/5.34/core_perl",
-                        "-Darchlib=/usr/lib/perl5/5.34/core_perl",
-                        "-Dsitelib=/usr/lib/perl5/5.34/site_perl",
-                        "-Dsitearch=/usr/lib/perl5/5.34/site_perl",
-                        "-Dvendorlib=/usr/lib/perl5/5.34/vendor_perl",
-                        "-Dvendorarch=/usr/lib/perl5/5.34/vendor_perl",
-                        "-Dman1dir=/usr/share/man/man1",
-                        "-Dman3dir=/usr/share/man/man3",
-                        "-Dpager=\"/usr/bin/less -isR\"",
-                        "-Duseshrplib",
-                        "-Dusethreads"])
+        runScript("Configure",[ "BUILD_ZLIB=False",
+                                "BUILD_BZIP2=0",
+                                "-des",
+                                "-Dprefix=/usr",
+                                "-Dvendorprefix=/usr",
+                                "-Dprivlib=/usr/lib/perl5/5.34/core_perl",
+                                "-Darchlib=/usr/lib/perl5/5.34/core_perl",
+                                "-Dsitelib=/usr/lib/perl5/5.34/site_perl",
+                                "-Dsitearch=/usr/lib/perl5/5.34/site_perl",
+                                "-Dvendorlib=/usr/lib/perl5/5.34/vendor_perl",
+                                "-Dvendorarch=/usr/lib/perl5/5.34/vendor_perl",
+                                "-Dman1dir=/usr/share/man/man1",
+                                "-Dman3dir=/usr/share/man/man3",
+                                "-Dpager=\"/usr/bin/less -isR\"",
+                                "-Duseshrplib",
+                                "-Dusethreads"],
+                                buildDirectoryPath)
     end
     
     def build
