bcd3106f2d7c90df7d85a9cfa9a56cda44965916
fulgurance <zohran.londais@gmail.com>
2023-02-16 11:10:43 +0000

    Patched few implementations error for e2fsprogs

--- E2fsprogs/1.46.4/1.46.4.cr
+++ E2fsprogs/1.46.4/1.46.4.cr
@@ -25,7 +25,7 @@ class Target < ISM::Software
     def prepareInstallation
         super
         makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
-        runGunzipCommand(["libext2fs.info.gz"],"#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/share/info/")
+        runGunzipCommand(["libext2fs.info.gz"],"#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/info/")
     end
 
     def install
